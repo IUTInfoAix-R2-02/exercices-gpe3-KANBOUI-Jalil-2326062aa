ef52baedb1339f3847deecd67aee8f56ea8de7e4
KANBOUI-Jalil-2326062aa <jalil.kanboui@etu.univ-amu.fr>
2024-04-15 09:36:47 +0200

    Commit exercice 4 complet

--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -1,8 +1,12 @@
 package fr.amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -25,6 +29,53 @@ public class Palette extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        root = new BorderPane();
+
+
+        vert = new Button("Vert");
+        rouge = new Button("Rouge");
+        bleu = new Button("Bleu");
+
+        bas = new HBox(10);
+        bas.getChildren().addAll(vert,rouge,bleu);
+        bas.setAlignment(Pos.CENTER);
+        bas.setPadding(new Insets(10));
+
+        panneau = new Pane();
+
+        label = new Label();
+        label.setStyle("-fx-font-weight: bold");
+        label.setStyle("-fx-font-size: 20");
+
+
+        vert.addEventFilter(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            nbVert += 1;
+            label.setText("Vert choisi " + nbVert + " fois");
+            panneau.setStyle("-fx-background-color : green");
+        });
+
+        rouge.addEventFilter(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            nbRouge += 1;
+            label.setText("Rouge choisi " + nbRouge + " fois");
+            panneau.setStyle("-fx-background-color : red");
+        });
+
+        bleu.addEventFilter(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            nbBleu += 1;
+            label.setText("Bleu choisi " + nbBleu + " fois");
+            panneau.setStyle("-fx-background-color : blue");
+        });
+
+
+        root.setTop(label);
+        root.setCenter(panneau);
+        root.setBottom(bas);
+        BorderPane.setAlignment(label,Pos.CENTER);
+
+
+
+        Scene scene = new Scene(root,400,200);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
